75784a9ea0046597c576054b0f4ae21484397131
Johannes Mols <johannes.mols@googlemail.com>
2020-09-21 20:23:15 +0200

    Created a very basic drone object

---
+++ Assets/Scripts/Objects/Drone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drone : MonoBehaviour
+{
+    public Transform location;
+    public double weight;
+    public double batteryCapacity;
+    public double chargingTime;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        location.Translate(Vector3.forward * Time.deltaTime);
+    }
+}
