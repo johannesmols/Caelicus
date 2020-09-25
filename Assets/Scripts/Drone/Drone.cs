c848d2d5506c35997426f50a28021dc52ef16dcb
Johannes Mols <johannes.mols@kraftvaerk.com>
2020-09-25 09:52:51 +0200

    Added states

---
+++ Assets/Scripts/Drone/Drone.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Assets.Scripts.Drone;
+using UnityEngine;
+
+namespace Assets.Scripts.Drone
+{
+    public class Drone : MonoBehaviour
+    {
+        // Public properties
+        public double Weight = 10d;
+        public double BatteryCapacity = 300d;
+        public double ChargingTime = 60d;
+
+        // Private properties
+        private ModeOfOperation _modeOfOperation;
+
+        void Start()
+        {
+            _modeOfOperation = ModeOfOperation.Idle;
+        }
+
+        void Update()
+        {
+            // just a test
+            // gameObject.transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+
+        public void ChangeModeOfOperation(ModeOfOperation mode, Vector3 target = new Vector3())
+        {
+            switch (mode)
+            {
+                case ModeOfOperation.Idle:
+                    break;
+                case ModeOfOperation.Charging:
+                    break;
+                case ModeOfOperation.Launch:
+                    break;
+                case ModeOfOperation.FlightToTarget:
+                    break;
+                case ModeOfOperation.FlightToBase:
+                    break;
+                case ModeOfOperation.Landing:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Drone/ModeOfOperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Drone
+{
+    public enum ModeOfOperation
+    {
+        Idle,
+        Charging,
+        Launch,
+        FlightToTarget,
+        FlightToBase,
+        Landing
+    }
+}
