8d136f3fc06df201dd0cc0989ca707986087d5b0
Johannes Mols <johannes.mols@kraftvaerk.com>
2020-09-25 10:10:32 +0200

    Added basic state switching

--- Assets/Scripts/Drone/Drone.cs
+++ Assets/Scripts/Drone/Drone.cs
@@ -7,31 +7,36 @@ namespace Assets.Scripts.Drone
     public class Drone : MonoBehaviour
     {
         // Public properties
-        public double Weight = 10d;
-        public double BatteryCapacity = 300d;
-        public double ChargingTime = 60d;
+        public float Weight = 10f;
+        public float BatteryCapacity = 300f;
+        public float ChargingSpeed = 1f;
 
         // Private properties
         private ModeOfOperation _modeOfOperation;
+        private Vector3 _currentTarget;
+        private float _batteryCharge;
 
         void Start()
         {
             _modeOfOperation = ModeOfOperation.Idle;
+            _batteryCharge = BatteryCapacity;
         }
 
         void Update()
         {
-            // just a test
-            // gameObject.transform.Translate(Vector3.forward * Time.deltaTime);
-        }
-
-        public void ChangeModeOfOperation(ModeOfOperation mode, Vector3 target = new Vector3())
-        {
-            switch (mode)
+            switch (_modeOfOperation)
             {
                 case ModeOfOperation.Idle:
                     break;
                 case ModeOfOperation.Charging:
+                    if (_batteryCharge >= BatteryCapacity)
+                    {
+                        _modeOfOperation = ModeOfOperation.Idle;
+                    }
+                    else
+                    {
+                        _batteryCharge += ChargingSpeed * Time.deltaTime; // charge battery by ChargingSpeed per second
+                    }
                     break;
                 case ModeOfOperation.Launch:
                     break;
@@ -43,6 +48,38 @@ namespace Assets.Scripts.Drone
                     break;
                 default:
                     break;
+            }
+        }
+
+        // Return value: success or failure to switch mode
+        public bool ChangeModeOfOperation(ModeOfOperation mode, Vector3 target = new Vector3())
+        {
+            switch (mode)
+            {
+                case ModeOfOperation.Idle:
+                    _modeOfOperation = mode;
+                    return true;
+                case ModeOfOperation.Charging:
+                    _modeOfOperation = mode;
+                    return true;
+                case ModeOfOperation.Launch:
+                    _modeOfOperation = mode;
+                    return true;
+                case ModeOfOperation.FlightToTarget:
+                    if (target == Vector3.zero) return false;
+                    _modeOfOperation = mode;
+                    _currentTarget = target;
+                    return true;
+                case ModeOfOperation.FlightToBase:
+                    if (target == Vector3.zero) return false;
+                    _modeOfOperation = mode;
+                    _currentTarget = target;
+                    return true;
+                case ModeOfOperation.Landing:
+                    _modeOfOperation = mode;
+                    return true;
+                default:
+                    return false;
             }
         }
     }
