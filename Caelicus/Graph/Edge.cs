92da0ea1762d43d0242a0a66ddeaf49c649faacf
Johannes Mols <johannes.mols@googlemail.com>
2020-10-24 01:04:54 +0200

    Added uGraph code

---
+++ Caelicus/Graph/Edge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caelicus.Graph
+{
+    public class Edge<TVertex, TEdge>
+    {
+        public Guid Id { get; }
+
+        public TEdge Info { get; set; }
+
+        public Vertex<TVertex, TEdge> Origin { get; internal set; }
+
+        public Vertex<TVertex, TEdge> Destination { get; internal set; }
+
+        internal Edge(TEdge info)
+        {
+            Id = Guid.NewGuid();
+            Info = info;
+        }
+    }
+}
---
+++ Caelicus/Graph/Graph.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caelicus.Graph
+{
+    /// <summary>
+    /// Generic Graph implementation based on Adjacency list
+    /// Source: https://github.com/leandroltavares/uGraph
+    /// </summary>
+    /// <typeparam name="TVertex"></typeparam>
+    /// <typeparam name="TEdge"></typeparam>
+    public class Graph<TVertex, TEdge> : IEnumerable<Vertex<TVertex, TEdge>>
+    {
+        public IReadOnlyList<Vertex<TVertex, TEdge>> Vertices => _vertices.AsReadOnly();
+
+        public int VertexCount => _verticesSet.Count;
+
+        public int EdgeCount { get; set; }
+
+
+        private readonly List<Vertex<TVertex, TEdge>> _vertices;
+
+        private readonly HashSet<Guid> _verticesSet;
+
+        public Graph()
+        {
+            _vertices = new List<Vertex<TVertex, TEdge>>();
+            _verticesSet = new HashSet<Guid>();
+        }
+
+        public void AddVertex(Vertex<TVertex, TEdge> vertex)
+        {
+            _vertices.Add(vertex);
+            _verticesSet.Add(vertex.Id);
+        }
+
+        public Vertex<TVertex, TEdge> AddVertex(TVertex info)
+        {
+            var vertex = new Vertex<TVertex, TEdge> { Info = info };
+
+            AddVertex(vertex);
+
+            return vertex;
+        }
+
+        public Edge<TVertex, TEdge> AddEdge(Vertex<TVertex, TEdge> origin, Vertex<TVertex, TEdge> destination, TEdge info)
+        {
+            var edge = new Edge<TVertex, TEdge>(info)
+            {
+                Origin = origin,
+                Destination = destination
+            };
+
+            origin.Edges.Add(edge);
+
+            EdgeCount++;
+
+            return edge;
+        }
+
+        public Edge<TVertex, TEdge> AddEdge<T>(T origin, T destination, TEdge info) where T : TVertex, IEquatable<TVertex>
+        {
+            var originVertex = FirstOrDefault(v => ((IEquatable<T>)(v)).Equals(origin));
+
+            if (originVertex == null)
+                throw new ArgumentNullException(nameof(originVertex));
+
+            var destinationVertex = FirstOrDefault(v => ((IEquatable<T>)(v)).Equals(destination));
+
+            if (destinationVertex == null)
+                throw new ArgumentNullException(nameof(destinationVertex));
+
+            return AddEdge(originVertex, destinationVertex, info);
+        }
+
+        public Vertex<TVertex, TEdge> FirstOrDefault(Func<TVertex, bool> predicate)
+        {
+            return _vertices.FirstOrDefault(v => predicate(v.Info));
+        }
+
+        public bool Contains<T>(T value) where T : TVertex, IEquatable<TVertex>
+        {
+            return _vertices.Any(v => v.Info.Equals(value));
+        }
+
+        public bool Contains(Vertex<TVertex, TEdge> vertex)
+        {
+            return _vertices.Any(v => v.Equals(vertex));
+        }
+
+        public IEnumerator<Vertex<TVertex, TEdge>> GetEnumerator()
+        {
+            return _vertices.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _vertices.GetEnumerator();
+        }
+
+        public bool Equals(Graph<TVertex, TEdge> other)
+        {
+            if (other == null)
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (VertexCount != other.VertexCount)
+                return false;
+
+            var thisOrderedVertices = this._vertices.GroupBy(v => v.Edges.Count());
+            var otherOrderedVertices = other._vertices.GroupBy(v => v.Edges.Count());
+
+            var otherGraphVisitedVertices = new HashSet<Vertex<TVertex, TEdge>>();
+
+            foreach (var vertexGroupPair in thisOrderedVertices.Zip(otherOrderedVertices, (first, second) => (first, second)))
+            {
+                foreach (var originVertex in vertexGroupPair.first)
+                {
+                    var matchingDestinationVertex =
+                        vertexGroupPair.second.FirstOrDefault(destinationVertex => destinationVertex.Info.Equals(originVertex.Info)
+                                                              && !otherGraphVisitedVertices.Contains(destinationVertex)
+                                                              && originVertex.Edges.Count() == destinationVertex.Edges.Count());
+
+                    if (matchingDestinationVertex != null)
+                    {
+
+                        otherGraphVisitedVertices.Add(matchingDestinationVertex);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return other._vertices.Count == otherGraphVisitedVertices.Count;
+        }
+
+        #region Private Methods
+
+        private void ClearVisitedVertices()
+        {
+            foreach (var vertex in _vertices)
+            {
+                vertex.Visited = false;
+            }
+        }
+
+        #endregion
+
+        #region Traversal
+
+        /// <summary>
+        /// Depth-first traversal
+        /// <paramref name="initialVertex">Initial vertex of the traversal</paramref>
+        /// <paramref name="action">Action to be executed foreach node</paramref>
+        /// </summary>
+        public void DepthFirstTraversal(Vertex<TVertex, TEdge> initialVertex, Action<Vertex<TVertex, TEdge>> action)
+        {
+            if (initialVertex == null)
+                throw new ArgumentNullException(nameof(initialVertex));
+
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            var stack = new Stack<Vertex<TVertex, TEdge>>();
+
+            ClearVisitedVertices();
+
+            stack.Push(initialVertex);
+
+            while (stack.Count > 0)
+            {
+                var currentVertex = stack.Pop();
+
+                if (!currentVertex.Visited)
+                {
+                    currentVertex.Visited = true;
+
+                    action(currentVertex);
+
+                    foreach (var edge in currentVertex.Edges)
+                    {
+                        stack.Push(edge.Destination);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Breadth-first traversal
+        /// <paramref name="initialVertex">Initial vertex of the traversal</paramref>
+        /// <paramref name="action">Action to be executed foreach node</paramref>
+        /// </summary>
+        public void BreadthFirstTraversal(Vertex<TVertex, TEdge> initialVertex, Action<Vertex<TVertex, TEdge>> action)
+        {
+            if (initialVertex == null)
+                throw new ArgumentNullException(nameof(initialVertex));
+
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            var queue = new Queue<Vertex<TVertex, TEdge>>();
+
+            ClearVisitedVertices();
+
+            queue.Enqueue(initialVertex);
+
+            while (queue.Count > 0)
+            {
+                var currentVertex = queue.Dequeue();
+
+                if (!currentVertex.Visited)
+                {
+                    currentVertex.Visited = true;
+
+                    action(currentVertex);
+
+                    foreach (var edge in currentVertex.Edges)
+                    {
+                        queue.Enqueue(edge.Destination);
+                    }
+                }
+            }
+        }
+
+        public bool BidirectionalSearch(Vertex<TVertex, TEdge> initialVertex, Vertex<TVertex, TEdge> targetVertex)//, out List<Vertex<TVertex, TEdge>> _vertices)
+        {
+            if (initialVertex == null)
+                throw new ArgumentNullException(nameof(initialVertex));
+
+            if (targetVertex == null)
+                throw new ArgumentNullException(nameof(targetVertex));
+
+            var queueA = new Queue<Vertex<TVertex, TEdge>>();
+            var queueB = new Queue<Vertex<TVertex, TEdge>>();
+            var visitedA = new HashSet<Vertex<TVertex, TEdge>>();
+            var visitedB = new HashSet<Vertex<TVertex, TEdge>>();
+
+            visitedA.Add(initialVertex);
+            visitedB.Add(targetVertex);
+
+            queueA.Enqueue(initialVertex);
+            queueB.Enqueue(targetVertex);
+
+            while (queueA.Count > 0 || queueB.Count > 0)
+            {
+                if (PathExistsBidirectionalHelper(queueA, visitedA, visitedB))
+                {
+                    return true;
+                }
+                if (PathExistsBidirectionalHelper(queueB, visitedB, visitedA))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool PathExistsBidirectionalHelper(Queue<Vertex<TVertex, TEdge>> queue, HashSet<Vertex<TVertex, TEdge>> visitedFromThisSide, HashSet<Vertex<TVertex, TEdge>> visitedFromThatSide)
+        {
+            if (queue.Count > 0)
+            {
+                var next = queue.Dequeue();
+
+                foreach (var adjacent in next.Edges.Select(s => s.Destination))
+                {
+                    if (visitedFromThatSide.Contains(adjacent))
+                    {
+                        return true;
+                    }
+
+                    if (visitedFromThisSide.Add(adjacent))
+                    {
+                        queue.Enqueue(adjacent);
+                    }
+                }
+            }
+            return false;
+        }
+
+        #endregion
+    }
+}
