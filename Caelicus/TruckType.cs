1b52cc6ba2207f2ea2622598827578415533f77b
Gustav  Weber Kinch <Gustavwk@gmail.com>
2020-10-27 10:05:47 +0100

    Truck Obejct
    Simple truck object - alot is still to be done but i have made a class based on the  https://www.first-transport.dk/services/priser-og-betingelser

---
+++ Caelicus/TruckType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caelicus.Services
+{
+    public enum TruckType
+    {
+        Pickup,
+        Lastvogn,
+        StorLastvogn,
+        Sættevogn,
+
+    }
+}
