32817278d61e7895bd7aed2561bbd1cd45c02a18
Johannes Mols <johannes.mols@googlemail.com>
2020-10-31 18:10:10 +0100

    Generating parameters to use in simulation

--- Caelicus/Simulation/SimulationParameters.cs
+++ Caelicus/Simulation/SimulationParameters.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Caelicus.Graph;
 using Caelicus.Models.Graph;
 using Caelicus.Models.Vehicles;
 
@@ -12,6 +13,10 @@ namespace Caelicus.Simulation
         /// <summary>
         /// Graph to use in the simulation
         /// </summary>
-        public JsonGraphRootObject Graph { get; set; } = new JsonGraphRootObject();
+        public Graph<VertexInfo, EdgeInfo> Graph { get; set; } = new Graph<VertexInfo, EdgeInfo>();
+
+        public List<Tuple<Vehicle, bool>> Vehicles { get; set; } = new List<Tuple<Vehicle, bool>>();
+
+        public float SimulationSpeed { get; set; } = 1f;
     }
 }
