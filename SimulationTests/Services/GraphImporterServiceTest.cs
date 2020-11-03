eb27718522158bab58cf32e56c7b23b69c3bc401
Gustavwk <37047018+Gustavwk@users.noreply.github.com>
2020-11-03 17:27:48 +0100

    Some further structure and testtemplates

---
+++ SimulationTests/Services/GraphImporterServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SimulationTests.Services
+{
+    public class GraphImporterServiceTest
+    {
+        [Fact]
+        public void calculateDirectDistanceTest() 
+        { 
+        }
+
+        [Fact]
+        public void generateGraphTest()
+        {
+        }
+
+        [Fact]
+        public void DetermineVertexTypeTest()
+        {
+        }
+    }
+
+}
