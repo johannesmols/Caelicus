5166e04e220913cac0878bc841094f0a05a5503a
Enrico Tolotto <etolotto@gmail.com>
2020-11-09 11:33:55 +0100

    unittests: more test added and update workflow

--- SimulationTests/SimulationTests.cs
+++ SimulationTests/SimulationTests.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Caelicus.Models.Graph;
 using Caelicus.Models.Vehicles;
 using Caelicus.Services;
 using Caelicus.Simulation;
+using Microsoft.VisualStudio.TestPlatform.Common.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 
@@ -13,26 +16,106 @@ namespace SimulationTests
     [TestClass]
     public class SimulationTests
     {
-        [TestMethod]
-        public void LoadJsonResources()
+        // Test fixture to pass around various tests
+        // I found this singleton class online, it should be thread safe... 
+        public sealed class SimResources
         {
-            // To load the data from the data resource just declare the resource file as txt file and load it
-            // by invoking data.file
-            var vehicle = JsonConvert.DeserializeObject<List<Vehicle>>(data.vehicles);
-            var graph = JsonConvert.DeserializeObject<JsonGraphRootObject>(data.copenhagen);
+            private static readonly SimResources instance = new SimResources();
 
-            var parameters = new SimulationParameters()
+            public List<Vehicle> Vehicle { get; } = JsonConvert.DeserializeObject<List<Vehicle>>(data.vehicles);
+
+            public JsonGraphRootObject Graph { get; } =
+                JsonConvert.DeserializeObject<JsonGraphRootObject>(data.copenhagen);
+
+            public SimulationParameters Parameters;
+
+            static SimResources()
             {
-                SimulationIdentifier = Guid.NewGuid(),
-                RandomSeed = 42,
-                Graph = new GraphImporterService().GenerateGraph(graph),
-                VehicleTemplate = vehicle[0],
-                NumberOfVehicles = 1,
-                SimulationSpeed = 10
-            };
+            }
 
-            Assert.AreEqual("Copenhagen", graph.Name);
-            Assert.AreEqual("Drone", vehicle[0].Name);
+            private SimResources()
+            {
+                Parameters = new SimulationParameters()
+                {
+                    SimulationIdentifier = Guid.NewGuid(),
+                    RandomSeed = 42,
+                    Graph = new GraphImporterService().GenerateGraph(Graph),
+                    VehicleTemplate = Vehicle[0],
+                    NumberOfVehicles = 1,
+                    SimulationSpeed = 10
+                };
+            }
+
+            public static SimResources Instance
+            {
+                get { return instance; }
+            }
+        }
+
+
+
+        [TestMethod]
+        public void TestLoadJsonResources()
+        {
+            var simResources = SimResources.Instance;
+            Assert.AreEqual("Copenhagen", simResources.Graph.Name);
+            Assert.AreEqual("Drone", simResources.Vehicle[0].Name);
+        }
+
+        [TestMethod]
+        public void TestGraph()
+        {
+            var simpParameters = SimResources.Instance.Parameters;
+            var jsonGraph = SimResources.Instance.Graph;
+            
+            foreach (var elem in jsonGraph.Vertices)
+            {
+                var testBase = elem.Name;
+                var testDestintion = elem.Edges;
+                foreach (var i in simpParameters.Graph)
+                {
+                    if (i.Info.Name == testBase)
+                    {
+                        // Check that the destinations listed int the json graph are the one added by the GraphImporter Service
+                        var currentDest = i.Edges.Select(x => x.Destination.Info.Name).ToList();
+                        CollectionAssert.AreEqual(testDestintion, currentDest, ToString());
+                        // Check that each destination has an edge with the current base
+                        foreach (var edge in i.Edges)
+                        {
+                            Console.WriteLine("Destination is = '" + edge.Destination.Info.Name + "' Base is = " + i.Info.Name); 
+                            var otherDest = edge.Destination.Edges.Select(x => x.Destination.Info.Name).ToList();
+                            foreach (var ii in otherDest)
+                            {
+                                Console.WriteLine(ii);
+                            }
+                            CollectionAssert.Contains(otherDest, i.Info.Name);
+                        }
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public async Task  TestStartStopSimulation()
+        {
+            var simParameters = SimResources.Instance.Parameters;
+            var simManager = new SimulationManager();
+            simManager.AddSimulation(simParameters);
+            var myres = simManager.StartSimulations();
+            Assert.IsTrue(myres.IsCompleted);
+             
+            var stopRes = await simManager.StartSimulations();
+            Assert.IsNull(stopRes.LastOrDefault());
+        }
+        
+        [TestMethod]
+        public async Task  TestAddMultipleSim()
+        {
+            var simParameters = SimResources.Instance.Parameters;
+            var simManager = new SimulationManager();
+            simManager.AddSimulation(simParameters);
+            simManager.AddSimulation(simParameters);
+            Assert.IsTrue(simManager.Simulations.Count == 2);
         }
     }
 }
