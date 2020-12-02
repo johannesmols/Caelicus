a0ce3f3a3f9517c04a32e814ccb65e325ed8b744
Johannes Mols <johannes.mols@googlemail.com>
2020-12-02 21:41:50 +0100

    Refactored unit tests

--- SimulationTests/SimulationTests.cs
+++ SimulationTests/SimulationTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -14,22 +15,15 @@ namespace SimulationTests
     [TestClass]
     public class SimulationTests
     {
-        // Test fixture to pass around various tests
-        // I found this singleton class online, it should be thread safe... 
         public sealed class SimResources
         {
-            private static readonly SimResources instance = new SimResources();
+            public static SimResources Instance { get; } = new SimResources();
 
             public List<Vehicle> Vehicle { get; } = JsonConvert.DeserializeObject<List<Vehicle>>(data.vehicles);
 
-            public JsonGraphRootObject Graph { get; } =
-                JsonConvert.DeserializeObject<JsonGraphRootObject>(data.copenhagen);
+            public JsonGraphRootObject Graph { get; } = JsonConvert.DeserializeObject<JsonGraphRootObject>(data.copenhagen);
 
             public SimulationParameters Parameters;
-
-            static SimResources()
-            {
-            }
 
             private SimResources()
             {
@@ -42,11 +36,6 @@ namespace SimulationTests
                     NumberOfVehicles = 1,
                     SimulationSpeed = 10
                 };
-            }
-
-            public static SimResources Instance
-            {
-                get { return instance; }
             }
         }
 
@@ -63,29 +52,31 @@ namespace SimulationTests
         [TestMethod]
         public void TestGraph()
         {
-            var simpParameters = SimResources.Instance.Parameters;
+            var simParameters = SimResources.Instance.Parameters;
             var jsonGraph = SimResources.Instance.Graph;
             
             foreach (var elem in jsonGraph.Vertices)
             {
                 var testBase = elem.Name;
-                var testDestintion = elem.Edges;
-                foreach (var i in simpParameters.Graph)
+                var testDestination = elem.Edges;
+
+                foreach (var i in simParameters.Graph)
                 {
                     if (i.Info.Name == testBase)
                     {
                         // Check that the destinations listed int the json graph are the one added by the GraphImporter Service
                         var currentDest = i.Edges.Select(x => x.Destination.Info.Name).ToList();
-                        CollectionAssert.AreEqual(testDestintion, currentDest, ToString());
+                        CollectionAssert.AreEqual(testDestination, currentDest, ToString());
                         // Check that each destination has an edge with the current base
                         foreach (var edge in i.Edges)
                         {
-                            Console.WriteLine("Destination is = '" + edge.Destination.Info.Name + "' Base is = " + i.Info.Name); 
+                            Console.WriteLine(@"Destination is = '" + edge.Destination.Info.Name + @"' Base is = " + i.Info.Name); 
                             var otherDest = edge.Destination.Edges.Select(x => x.Destination.Info.Name).ToList();
                             foreach (var ii in otherDest)
                             {
                                 Console.WriteLine(ii);
                             }
+
                             CollectionAssert.Contains(otherDest, i.Info.Name);
                         }
                     }
@@ -93,9 +84,9 @@ namespace SimulationTests
             }
         }
 
-        
+
         [TestMethod]
-        public async Task  TestAddMultipleSim()
+        public void TestAddMultipleSim()
         {
             var simParameters = SimResources.Instance.Parameters;
             var simManager = new SimulationManager();
@@ -108,11 +99,11 @@ namespace SimulationTests
         public void TestShortPath()
         {
             var graph = SimResources.Instance.Parameters.Graph;
-            List<string> firstPath = new List<string>(){"DTU", "Ballerup", "Glostrup"};
+            var firstPath = new List<string>(){"DTU", "Ballerup", "Glostrup"};
             var path = graph.FindShortestPath(graph , graph.Vertices[0], graph.Vertices[1]);
             var collect = path.Item1.Select(v => v.Info.Name).ToList();
             CollectionAssert.AreEqual(collect, firstPath);            
-            List<string> secondPath = new List<string>(){"Dragor", "Brondby Strand", "Glostrup"};
+            var secondPath = new List<string>(){"Dragor", "Brondby Strand", "Glostrup"};
             path = graph.FindShortestPath(graph , graph.Vertices[4], graph.Vertices[1]);
             collect = path.Item1.Select(v => v.Info.Name).ToList();
             CollectionAssert.AreEqual(collect, secondPath);            
@@ -121,29 +112,29 @@ namespace SimulationTests
         [TestMethod]
         public void TestVehicleDistribution()
         {
-            int bigNumber = 10000;
+            const double bigNumber = 10000;
             var parameters = SimResources.Instance.Parameters;
-            parameters.NumberOfVehicles = bigNumber;
+            parameters.NumberOfVehicles = (int) bigNumber;
             var sim = new Simulation(parameters);
             var base1 = parameters.Graph.Vertices[0];
             var base2 = parameters.Graph.Vertices[1];
             
-            int base1_count = 0;
-            int base2_count = 0;
+            var base1Count = 0;
+            var base2Count = 0;
             
             foreach (var v in sim.Vehicles)
             {
                 if (v.CurrentVertexPosition == base1)
                 {
-                    base1_count++;
+                    base1Count++;
                 } else if (v.CurrentVertexPosition == base2)
                 {
-                    base2_count++;
+                    base2Count++;
                 }
             }
             
-            Assert.IsTrue(base1_count > (bigNumber / 2) * 0.9);
-            Assert.IsTrue(base2_count > (bigNumber / 2) * 0.9);
+            Assert.IsTrue(base1Count > bigNumber / 2 * 0.9);
+            Assert.IsTrue(base2Count > bigNumber / 2 * 0.9);
         }
         
     }
